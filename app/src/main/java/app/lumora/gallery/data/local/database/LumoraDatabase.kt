7913a424a7ff831a90f9566a9563e9e3aa0baf75
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-09 19:17:32 +0600

    Disable Room schema export until schema directory is configured

--- app/src/main/java/app/lumora/gallery/data/local/database/LumoraDatabase.kt
+++ app/src/main/java/app/lumora/gallery/data/local/database/LumoraDatabase.kt
@@ -3,7 +3,7 @@ package app.lumora.gallery.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [MediaEntity::class], version = 1, exportSchema = true)
+@Database(entities = [MediaEntity::class], version = 1, exportSchema = false)
 abstract class LumoraDatabase : RoomDatabase() {
     abstract fun mediaDao(): MediaDao
 }
